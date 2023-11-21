049bf8fe01ac4b21992312a9d2e57f99b7ab5059
MARWAHAMED629 <mmero385@gmail.com>
2023-11-21 12:53:10 -0800

    add task files

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """  this class that defines properties of square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class of Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (init): The size of the new square.
+        """
+        self.__size = size
