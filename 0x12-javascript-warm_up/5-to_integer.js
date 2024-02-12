62ee2e9e2b4a685632ff0c93fcb6bc9da7e5faba
MARWAHAMED629 <mmero385@gmail.comFix_My_Code_Challenge.git>
2024-02-12 04:06:54 -0800

    5 to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const n = math.floor(Number(process.argv[2]));
-console.log(isNaN(n) ? 'Not a Number' : 'My number : ${n}');
+if (isNaN(process.argv[2])) {
+	  console.log('Not a number');
+} else {
+	  console.log('My number: ' + parseInt(process.argv[2]));
+}
