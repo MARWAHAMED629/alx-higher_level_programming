57ae2bb4c839dc074986e20cccfdfc07159d6ff4
MARWAHAMED629 <mmero385@gmail.comFix_My_Code_Challenge.git>
2024-02-13 11:46:49 -0800

    update 101 sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,7 +2,7 @@
 const { dict } = require('./101-data');
 const n_dict = {};
 for (const key in dict) {
-  if (dict[key] in newdict) {
+  if (dict[key] in n_dict) {
     n_dict[dict[key]].push(key);
   } else {
     n_dict[dict[key]] = [key];
