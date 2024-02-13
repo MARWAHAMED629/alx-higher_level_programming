f3a4d5a8093a17fc0d19bef5da5612392514ddc4
MARWAHAMED629 <mmero385@gmail.comFix_My_Code_Challenge.git>
2024-02-13 12:21:42 -0800

    update 102 concat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-let dataFile1 = '';
-let dataFile2 = '';
-//  Gathering data
-try {
-  dataFile1 = fs.readFileSync(process.argv[2], 'utf8');
-  dataFile2 = fs.readFileSync(process.argv[3], 'utf-8');
-} catch (err) {
-  console.error(err);
-}
-// Writting file
-const writeToFile = (data) => {
-  fs.appendFile(process.argv[4], data + '\n', (err) => {
-    if (err) {
-      return console.error(err);
-    }
-  });
-};
-writeToFile(dataFile1);
-writeToFile(dataFile2);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
