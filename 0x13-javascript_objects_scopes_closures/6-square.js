07726f4ddfb5dfdc3d7ee7f4a05ab6045b8dcdc7
MARWAHAMED629 <mmero385@gmail.comFix_My_Code_Challenge.git>
2024-02-13 10:28:27 -0800

    update 6-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,17 +2,18 @@
 const SquareP = require('./5-square');
 
 class Square extends SquareP {
-  if (c === undefined) {
-    c = 'X';
-  }
-  for (let i = 0; i < this.height; i++) {
-    let s = '';
-    for (let j = 0; j < this.width; j++) {
-      s += c;
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
-    console.log(s);
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
   }
- }
 }
 
 module.exports = Square;
