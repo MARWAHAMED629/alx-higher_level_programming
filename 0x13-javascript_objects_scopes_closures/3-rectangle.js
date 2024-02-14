a413705130a250b4cb1a01e46877181a6ed32d52
MARWAHAMED629 <59849322+MARWAHAMED629@users.noreply.github.com>
2024-02-14 13:33:09 +0200

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,14 +6,15 @@ class Rectangle {
       this.height = h;
     }
   }
+
   print () {
-     for (let i = 0; i < this.height; i++) {
-       let s = '';
-       for (let j = 0; j < th  }is.width; j++) {
-	 s += 'X';
-       }
-       console.log(s);
-     }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
+    }
   }
 }
 
