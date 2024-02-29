4a34ca9077dd25d36079fe6c1fe9dbbf7028718b
MARWAHAMED629 <mmero385@gmail.com>
2024-02-29 15:07:00 -0800

    update catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Send a POST request with an empty body to 0.0.0.0:5000/catch_me
-curl -X POST -d '' http://0.0.0.0:5000/catch_me
-
-# Exit script without printing anything (only successful exit code 0 is shown)
-exit 0
+# Script that makes a request to causes an specific response
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "You got me!"
